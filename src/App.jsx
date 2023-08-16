549abc2da97a067baba1f1a7b51dc5c45d49803d
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-08-15 20:44:08 -0700

    Header created for mobile size

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
-import './Header.scss';
+import logo from '../../assets/logos/2.svg';
+import menuIcon from '../../assets/icons/menu.svg';
 
+import './Header.scss'
+import { useState } from 'react';
 
 const Header = () => {
-    return (
 
+    const [menuOpen, setMenuOpen] = useState(false)
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <img src={logo} alt="" className="header__logo" />
+                <div className={menuOpen ? 'header__menu header__menu--close' : "header__menu"} onClick={() => {
+                    setMenuOpen(!menuOpen)
+                }}></div>
+            </div>
+            <nav className={menuOpen ? 'navbar navbar--close' : "navbar"}>
+                <ul className='navbar__list'>
+                    <li className="navbar__item">
+                        <a className="navbar__link">Browse tasks</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a className="navbar__link">How it works</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a className="navbar__link">Sign up</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a className="navbar__link">Log in</a>
+                    </li>
+                    <li className="navbar__item">
+                        <button className="button">Become a Task helper</button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
