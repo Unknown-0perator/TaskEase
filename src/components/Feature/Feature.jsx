c8fd83c8060d85b89ed991027691d751f31c3345
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-08-18 15:55:34 -0700

    feature branch

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,9 +1,11 @@
 import './Feature.scss';
+import FeatureCard from '../FeatureCard/FeatureCard';
 
 const Feature = () => {
     return (
         <section className="feature">
-            <h1 className="feature__heading">Our Features</h1>
+            <h1 className="feature__heading">How it works</h1>
+            <FeatureCard />
         </section>
     )
 }
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import './LandingPage.scss'
 import Banner from '../../components/Banner/Banner';
+import Feature from '../../components/Feature/Feature';
 
 
 const LandingPage = () => {
     return (
         <div>
             <Banner />
+            <Feature />
         </div>
     )
 }
