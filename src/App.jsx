231c1d6bcc785fb81f49b152a1df3c15e152f40b
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-08-19 13:00:00 -0700

    Login page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='sign-up' element={<SignUp />} />
+        <Route path='login' element={<Login />} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
 
